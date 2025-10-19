453ca263d11d5638bb6241f00a70feef49f8087a
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-19 03:34:41 +0300

    Noble titles, land titles, land holding, and some prince specs.

--- src/society/nobility.rs
+++ src/society/nobility.rs
@@ -1,26 +1,44 @@
 //! 758: Nobility
-
+//! 871: Special Titles for Nobility
+//! 
 use dicebag::DiceExt;
 use serde::{Deserialize, Serialize};
 
-use rpgassist::gender::Gender;
+use rpgassist::{gender::Gender, modifier::HasModifier};
 
 use crate::society::culture::{CultureLevelType, Level};
 
 /// A struct to haul around a barebones Noble NPC.
 #[derive(Debug, Clone)]
 pub struct NobleNPC {
-    gender: Gender,
-    culture: Level,
-    //wealth: Wealth,
-    title: Title,
+    pub gender: Gender,
+    pub culture: Level,
+    //pub wealth: Wealth,
+    pub title: Title,
 }
 
+/// A full blown nobility descriptor.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Nobility {
+    pub title: Title,
+    timod: i32,
+    /// Land titles — e.g. "Lord Regent", "Marshal of ...", etc. — if any.
+    pub land_titles: Option<Vec<String>>,
+    /// Land holdings in km², if any.
+    pub land_holdings: Option<i32>,
+}
+
+/// Noble title.
 #[derive(Debug, Deserialize, Serialize, PartialEq, PartialOrd, Clone)]
 pub enum Title {
     Hetman,
     Knight,
-    Prince,
+    /// Prince is a special case; T-758A(†
+    Prince {
+        /// Parent title. Relevant only when Prince's specs are a fraction of the parents'.
+        parent_title: Option<Box<Title>>,
+        fraction_owned: f64,
+    },
     Baronet,
     Baron,
     Count,
@@ -40,6 +58,7 @@ pub enum Title {
 }
 
 impl Title {
+    /// Generate a random noble title based on given [culture type][CultureLevelType].
     pub fn new(culture_type: &CultureLevelType) -> Self {
         match culture_type {
             CultureLevelType::Primitive =>
@@ -66,7 +85,7 @@ impl Title {
                     ..=60 => Self::Jarl,
                     ..=70 => Self::Subchieftain,
                     ..=75 => Self::Baron,
-                    ..=80 => Self::Prince,
+                    ..=80 => Self::mk_prince(culture_type),
                     _ => Self::Hetman
                 },
 
@@ -83,9 +102,220 @@ impl Title {
                     ..=60 => if 1.d2() == 1 {Self::Count} else {Self::Earl},
                     ..=75 => Self::Baron,
                     ..=78 => Self::Baronet,
-                    ..=90 => Self::Prince,
+                    ..=90 => Self::mk_prince(culture_type),
                     _ => Self::Knight
                 }
         }
+    }
+
+    fn mk_prince(culture_type: &CultureLevelType) -> Self {
+        let parent_title = Self::new_prince_parent(culture_type);
+        let fraction_owned = if let Some(_) = parent_title { 1.d10() as f64 * 0.1 } else { 1.0 };
+        Self::Prince { parent_title, fraction_owned }
+    }
+
+    /// Generate (optional) parents' title for a prince.
+    fn new_prince_parent(culture_type: &CultureLevelType) -> Option<Box<Self>> {
+        // 1..20 = archduke equivalent, but 21+ parent's title matters instead.
+        if 1.d100() > 20 {
+            let mut parent = Self::new(culture_type);
+            loop {
+                match parent {
+                    Self::Prince {..} => {
+                        log::debug!("Parent would've been a prince(ss) — redoing…");
+                        parent = Self::new(culture_type);
+                    },
+                    _ => break None
+                }
+            }
+        } else { None }
+    }
+}
+
+impl HasModifier for Nobility {
+    fn modifier(&self) -> i32 {
+        self.timod
+    }
+}
+
+impl Title {
+    /// FYI: for internal/generation-phase use only, not for live querying!
+    // NOTE: if the prince(ss)'s parents are prince-equivalent,
+    //       the TiMod will be miniscule at best… But so it goes.
+    fn modifier(&self) -> i32 {
+        match self {
+            Self::Emperor   => 60,
+            Self::HighKing  => 5.d10(),
+            Self::King      => 39,
+            Self::Kahn      => 5.d8(),
+            Self::RoyalPrince |
+            Self::Archduke  => 4.d10(),
+            Self::Duke      => 4.d8(),
+            Self::Marquis   => 3.d10(),
+            Self::Chieftain |
+            Self::Jarl      |
+            Self::Count     |
+            Self::Earl      => 3.d6(),
+            Self::Viscount  => 3.d8(),
+            Self::Subchieftain |
+            Self::Knight    => 2.d6(),
+            Self::Baron     => 2.d10(),
+            Self::Baronet   => 2.d8(),
+            Self::Prince { parent_title: Some(p_title), fraction_owned } => {
+                let m = p_title.modifier() as f64;
+                (m * fraction_owned) as i32 // NOTE: trunc is fine as rounding is irrelevant.
+            },
+            Self::Prince {..} => 4.d10(),
+            Self::Hetman      => 1.d6()
+        }
+    }
+
+    /// Generate (potential) land title(s) that the corresponding Title might have.
+    fn mk_land_titles(&self) -> Option<Vec<String>> {
+        match self {
+            Self::Chieftain    |
+            Self::Jarl         |
+            Self::Subchieftain |
+            Self::Prince {..}  |
+            Self::Hetman      => None,
+            Self::Emperor     => Some(mk_land_titles_vec(1.d4() + 3)),
+            Self::HighKing    => Some(mk_land_titles_vec(1.d6())),
+            Self::King        => Some(mk_land_titles_vec(1.d4() + 1)),
+            Self::Kahn        => Some(mk_land_titles_vec(1.d6())),
+            Self::RoyalPrince => Some(mk_land_titles_vec(1.d4())),
+            Self::Archduke    => Some(mk_land_titles_vec(1.d3() + 1)),
+            Self::Duke        => Some(mk_land_titles_vec(1.d3())),
+            Self::Marquis     => Some(mk_land_titles_vec(1.d2())),
+            Self::Viscount    => Some(mk_land_titles_vec(1)),
+            Self::Count        |
+            Self::Earl        => if 1.d100() < 91 {Some(mk_land_titles_vec(1))} else {None},
+            Self::Baron       => if 1.d100() < 76 {Some(mk_land_titles_vec(1))} else {None},
+            Self::Baronet     => if 1.d100() < 51 {Some(mk_land_titles_vec(1))} else {None},
+            Self::Knight      => if 1.d100() < 36 {Some(mk_land_titles_vec(1))} else {None},
+        }
+    }
+
+    /// Generate land holdings side in km², if any.
+    fn mk_land_holdings(&self) -> Option<i32> {
+        match self {
+            Self::Emperor      => Some(1.d20() * 10),
+            Self::HighKing     => if 1.d100() < 86 {Some(1.d20() * 5)} else {None},
+            Self::King         => Some(1.d10() * 10),
+            Self::Kahn         => if 1.d100() < 31 {Some(1.d10() * 5)} else {None},
+            Self::RoyalPrince  => if 1.d100() < 71 {Some(1.d20() * 5)} else {None},
+            
+            Self::Prince { parent_title: None,.. } |
+            Self::Archduke     => if 1.d100() < 76 {Some(1.d10() * 5)} else {None},
+
+            Self::Duke         => if 1.d100() < 86 {Some(1.d10() * 5)} else {None},
+            Self::Marquis      => if 1.d100() < 61 {Some(1.d20() + 12)} else {None},
+            Self::Chieftain    => if 1.d100() < 41 {Some(2.d6() + 8)} else {None},
+            Self::Viscount     => if 1.d100() < 51 {Some(1.d20() + 10)} else {None},
+            Self::Jarl         => if 1.d100() < 71 {Some(1.d6() + 4)} else {None},
+            Self::Subchieftain => if 1.d100() < 31 {Some(1.d8())} else {None},
+            Self::Count |
+            Self::Earl         => if 1.d100() < 41 {Some(1.d20() + 4)} else {None},
+            Self::Baron        => if 1.d100() < 61 {Some(1.d10() + 4)} else {None},
+            Self::Baronet      => if 1.d100() < 31 {Some(1.d10())} else {None},
+            Self::Knight       => if 1.d100() < 61 {Some(1.d4())} else {None},
+            Self::Hetman       => if 1.d100() < 86 {Some(1.d4())} else {None},
+            Self::Prince { parent_title: Some(p_title), fraction_owned } => {
+                if let Some(land_holdings) = p_title.mk_land_holdings() {
+                    Some((land_holdings as f64 * fraction_owned) as i32)
+                } else {
+                    None
+                }
+            },
+        }
+    }
+}
+
+/// Generate solid land title(s) vec.
+fn mk_land_titles_vec(count: i32) -> Vec<String> {
+    let mut titles = vec![];
+    for _ in 0..=count {
+        titles.push(mk_land_title());
+    }
+    titles
+}
+
+const LAND_TITLE_PART1: [&'static str; 20] = [
+    "Commander",
+    "Custodian",
+    "Grim Sentinel",
+    "High Champion",
+    "Honored Defender",
+    "Iron Tower",
+    "Lord Protector",
+    "Liberator",
+    "Lord Governor",
+    "Lord Guardian",
+    "Keeper",
+    "Preserver",
+    "Marshall",
+    "Ranger",
+    "Regent",
+    "Retaliator",
+    "Swordmaster",
+    "Vindicator",
+    "Warden",
+    "Watchwarder",
+];
+const LAND_TITLE_PART2: [&'static str; 11] = [
+    "Highland",
+    "Lowland",
+    "Upper",
+    "Lower",
+    "Seaward",
+    "Northern",
+    "Eastern",
+    "Southern",
+    "Western",
+    "Frozen",
+    "Scorched",
+];
+const LAND_TITLE_PART3: [&'static str; 20] = [
+    "Coasts",
+    "Creation",
+    "Domain",
+    "Downs",
+    "Fens",
+    "Forests",
+    "Garth",
+    "Heath",
+    "Hills",
+    "Isles",
+    "Marches",
+    "Moors",
+    "Mountains",
+    "Pale",
+    "Reaches",
+    "Shire",
+    "Steppe",
+    "Uplands",
+    "Wastes",
+    "Waves",
+];
+
+/// Generate single land title.
+fn mk_land_title() -> String {
+    let part1 = LAND_TITLE_PART1[1.d20()-1];
+    let r = 1.d(10 + LAND_TITLE_PART2.len());
+    if r < 11 { return part1.into() }
+    format!("{} of the {} {}",
+            part1,
+            LAND_TITLE_PART2[r - 11],
+            LAND_TITLE_PART3[1.d(LAND_TITLE_PART3.len()) - 1]
+        )
+}
+
+impl Nobility {
+    pub fn new(culture_type: &CultureLevelType) -> Self {
+        let title = Title::new(culture_type);
+        let timod = title.modifier();
+        let land_titles = title.mk_land_titles();
+        let land_holdings = title.mk_land_holdings();
+
+        Self { title, timod, land_titles, land_holdings }
     }
 }
