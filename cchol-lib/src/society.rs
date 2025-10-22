0cccb0bfeea18aabb324d55339cc38f604938ad7
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-22 06:57:28 +0300

    localized the mk_illegitimacy_reason()

--- cchol-lib/src/society.rs
+++ cchol-lib/src/society.rs
@@ -1,6 +1,7 @@
 pub mod birth;
 pub mod culture;
 pub mod environment;
+pub mod family;
 pub mod nobility;
 pub mod status;
 pub mod wealth;
--- cchol-lib/src/society/birth.rs
+++ cchol-lib/src/society/birth.rs
@@ -30,18 +30,18 @@ impl LegitMod for IllegitimacyReason {
 /// 
 /// Birth is considered legit if `None` is returned as a reason.
 pub fn determine_birth_illegitimacy(culture_type: &CultureLevelType) -> Option<IllegitimacyReason> {
+    // Generate a culturally biased illegitimacy reason.
+    fn mk_illegitimacy_reason(culture_type: &CultureLevelType) -> IllegitimacyReason {
+        match 1.d20() + culture_type.cumod() {
+            ..=12 => IllegitimacyReason::R1(1.d4()),
+            ..=14 => IllegitimacyReason::R2 { legit_mod: 1.d4(), father_known: 1.d100() < 16 },
+            ..=23 => IllegitimacyReason::R3 { legit_mod: 1.d4(), father_known: 1.d100() < 51 },
+            _     => IllegitimacyReason::R4 { legit_mod: 1.d4(), father_known: 1.d100() < 51 },
+        }
+    }
+
     match culture_type {
         CultureLevelType::Primitive => if 1.d20() == 20 { Some(mk_illegitimacy_reason(culture_type))} else { None },
         _ => if 1.d20() + culture_type.cumod() > 19 { Some(mk_illegitimacy_reason(culture_type))} else { None },
-    }
-}
-
-/// Generate a culturally biased illegitimacy reason.
-fn mk_illegitimacy_reason(culture_type: &CultureLevelType) -> IllegitimacyReason {
-    match 1.d20() + culture_type.cumod() {
-        ..=12 => IllegitimacyReason::R1(1.d4()),
-        ..=14 => IllegitimacyReason::R2 { legit_mod: 1.d4(), father_known: 1.d100() < 16 },
-        ..=23 => IllegitimacyReason::R3 { legit_mod: 1.d4(), father_known: 1.d100() < 51 },
-        _     => IllegitimacyReason::R4 { legit_mod: 1.d4(), father_known: 1.d100() < 51 },
     }
 }
