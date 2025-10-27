ef0dfe17f848ea1d1472e26ecc58dbcc58fc485d
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-28 01:55:04 +0200

    default "roll_range()" impl for anything with "_cr_range" in the struct.

--- cchol-lib/src/traits/roll_range.rs
+++ cchol-lib/src/traits/roll_range.rs
@@ -1,3 +1,15 @@
+/// A trait for anything that needs rolls within a range.
 pub trait HasRollRange {
+    /// Get roll range.
     fn roll_range(&self) -> &std::ops::RangeInclusive<i32>;
+}
+
+/// Unified default impl of [HasRollRange] for anything with a suitable `_cr_range` field.
+#[macro_export]
+macro_rules! default_roll_range_def {
+    ($for:ident) => {
+        impl HasRollRange for $for {
+            fn roll_range(&self) -> &std::ops::RangeInclusive<i32> { &self._cr_range }
+        }
+    };
 }
