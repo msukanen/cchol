4c606b63a2026c6645ba2e223c4514bebe28a286
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-28 01:58:12 +0200

    dice size determination macro to figure out what sort of a die to use for rolls.

--- cchol-lib/src/lib.rs
+++ cchol-lib/src/lib.rs
@@ -4,4 +4,16 @@ pub(crate) mod serialize;
 pub mod skill;
 pub mod social;
 mod traits;
-pub use traits::Named;
+pub use traits::IsNamed;
+
+#[macro_export]
+macro_rules! create_dice_size {
+    ($table:ident) => {
+        {
+            $table.iter()
+                .map(|e| *e.roll_range().end())
+                .max()
+                .unwrap_or_else(|| panic!("{} list is empty?!", stringify!($table)))
+        } as usize
+    };
+}
