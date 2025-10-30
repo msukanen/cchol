05bceb5fe05bcc2f7856d3daf6d759a64e9c6320
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-30 04:47:11 +0200

    personality traits to-do

--- cchol-lib/src/traits.rs
+++ cchol-lib/src/traits.rs
@@ -1,8 +1,10 @@
+mod described;
+pub use described::HasDescription;
+
 mod named;
 pub use named::IsNamed;
 
+pub mod personality;
+
 mod roll_range;
 pub use roll_range::HasRollRange;
-
-mod described;
-pub use described::HasDescription;
