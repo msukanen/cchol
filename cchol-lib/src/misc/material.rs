ab92da5dae341abcdb635cacd46eb63377f07156
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-07 12:42:21 +0200

    'strum' the materials randomizing.

--- cchol-lib/src/misc/material.rs
+++ cchol-lib/src/misc/material.rs
@@ -1,38 +1,34 @@
 use std::collections::HashSet;
 
 use dicebag::DiceExt;
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
+use strum::{EnumIter, IntoEnumIterator};
 
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Hash)]
+lazy_static! {
+    static ref SUBSTANCE_COUNT: usize = Substance::iter().count();
+}
+/// Various substances. Some more exotic/unusual than others…
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Hash, EnumIter)]
 pub enum Substance {
-    Granite, Marble,
-    Wood, IronWood,
-    PreciousMetal,
-    Cloth,
-    Gemstone,
-    Iron,
-    Bronze
+    Granite, Marble, Wood, IronWood, PreciousMetal,
+    Cloth, Gemstone, Iron, Bronze
 } impl Substance {
     pub fn random() -> HashSet<Self> {
         let mut set = HashSet::new();
         let mut i = 1;
-        while i > 0 && set.len() < 9 {
+        // loop around as long as we have to and we've no exhausted ALL options...
+        while i > 0 && set.len() < *SUBSTANCE_COUNT {
             i -= 1;
-            let matter = match 1.d10() {
-                ..=1 => Self::Granite,
-                2 => Self::Marble,
-                3 => Self::Wood,
-                4 => Self::IronWood,
-                5 => Self::PreciousMetal,
-                6 => Self::Cloth,
-                7 => Self::Gemstone,
-                8 => Self::Iron,
-                9 => Self::Bronze,
-                _ => {
-                    i += 2;
-                    continue;
-                }
-            };
+            let roll = 1.d(*SUBSTANCE_COUNT + 1);
+            if roll > *SUBSTANCE_COUNT {
+                // more than 1 material
+                i += 2;
+                continue;
+            }
+
+            let matter = Substance::iter().nth(roll - 1).unwrap();
+            // reroll if already present.
             if set.contains(&matter) {
                 i += 1;
                 continue;
