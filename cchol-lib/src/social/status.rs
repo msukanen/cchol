d8f1ea9851c468ea677e34a68f445c75c786cb61
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-08 11:05:07 +0200

    impl elevation to nobility (or increasing rank of current nobility); impl stripping of titles. Impl calculation of SolMod ...

--- cchol-lib/src/social/status.rs
+++ cchol-lib/src/social/status.rs
@@ -4,7 +4,7 @@ use dicebag::DiceExt;
 use rpgassist::resolve::resolve_in_place::ResolveInPlace;
 use serde::{Deserialize, Serialize};
 
-use crate::{modifier::CuMod, social::{culture::{Culture, HasCultureCoreType}, nobility::Noble, wealth::Wealth}};
+use crate::{modifier::CuMod, social::{SolMod, culture::{Culture, HasCultureCoreType}, nobility::Noble, wealth::Wealth}};
 
 /// Status specs.
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -36,5 +36,28 @@ pub struct SocialStatus {
 
     pub fn nobility(&self) -> Option<&Noble> {
         self.nobility.as_ref()
+    }
+
+    /// Elevate to next higher nobility rung…
+    pub fn elevate_nobility(&mut self, culture: &Culture) {
+        if let Some(n) = &self.nobility {
+            if let Some(next) = n.get_next_higher_rank(culture) {
+                self.nobility = Some(next.into())
+            }
+        } else {
+            self.nobility = Some(Noble::from(Noble::get_lowest_rank(culture)))
+        }
+    }
+
+    /// There goes the noble titles, buh bye…
+    pub fn demote_nobility(&mut self) {
+        self.nobility = None
+    }
+}
+
+impl SolMod for SocialStatus {
+    fn solmod(&self) -> i32 {
+        // being a noble (born as such or otherwise) adds a flat '5' to SolMod value.
+        self.wealth.solmod() + if let Some(_) = &self.nobility {5} else {0}
     }
 }
